d1cda98ba87a406e45c0d8fd02d9e3602d06c994
Madli Reiljan <madlireiljan@gmail.com>
2024-09-19 10:53:25 +0300

    Get article data by slug

--- controllers/article.js
+++ controllers/article.js
@@ -3,6 +3,7 @@ const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequeli
 
 const Article = require('../models/article')(sequelize, Sequelize.DataTypes);
 
+//get all data from table
 const getAllArticles = (req, res) => {
     Article.findAll()
     .then(articles => {
@@ -14,6 +15,23 @@ const getAllArticles = (req, res) => {
     })
 }
 
+//show article by this slug
+const getArticleBySlug = (req, res) => {
+    Article.findOne({
+        where: {
+            slug : req.params.slug
+        }
+    })
+    .then(article => {
+        console.log(article)
+        return res.status(200).json({ article });
+    })
+    .catch (error => {
+        return res.status(500).send(error.message);
+    })
+}
+
 module.exports = {
-    getAllArticles
+    getAllArticles,
+    getArticleBySlug
 };
