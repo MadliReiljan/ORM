552012f98c62135a3f9702619be2aa7916fcd5ff
Madli Reiljan <madlireiljan@gmail.com>
2024-09-19 09:15:13 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+//Connect to database
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database', err);
+    });
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to sequelize application. "});
 });
